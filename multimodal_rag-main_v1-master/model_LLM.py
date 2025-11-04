7cf8e9dff1e3c13ecc1d55d177428c295d79c8a6
vigorlee <vigorlee@users.noreply.github.com>
2025-11-04 15:36:13 +0800

    feat: add all project files - multimodal RAG implementation

---
+++ multimodal_rag-main_v1-master/model_LLM.py
@@ -0,0 +1,350 @@
+import json
+import re
+from typing import Optional, Any
+
+import torch
+from PIL import Image
+from llama_index.core.base.llms.types import LLMMetadata, CompletionResponse, ChatResponse, CompletionResponseGen
+from llama_index.core.llms import CustomLLM
+from llama_index.core.llms.callbacks import llm_completion_callback
+from transformers import TextStreamer, AutoModel, AutoTokenizer, LlamaTokenizer, LlamaForCausalLM, AutoModelForCausalLM
+
+from LLM.MobileVLM.mobilevlm.conversation import SeparatorStyle
+from LLM.SliME.llava.mm_utils import get_model_name_from_path as get_model_name_from_path_SLIME
+from LLM.mipha.constants import IMAGE_TOKEN_INDEX, DEFAULT_IM_START_TOKEN, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_END_TOKEN
+from LLM.mipha.conversation import conv_templates
+from LLM.mipha.mm_utils import tokenizer_image_token, process_images, KeywordsStoppingCriteria
+from LLM.mipha.model.builder import load_pretrained_model
+from LLM.MobileVLM.mobilevlm.model.mobilevlm import load_pretrained_model as load_pretrained_model_mobileVLM
+from LLM.MobileVLM.mobilevlm.conversation import conv_templates as conv_templates_mobileVLM
+from LLM.MobileVLM.mobilevlm.utils import process_images as process_images_mobileVLM
+from LLM.MobileVLM.mobilevlm.utils import tokenizer_image_token as tokenizer_image_token_mobileVLM
+from LLM.MobileVLM.mobilevlm.utils import KeywordsStoppingCriteria as KeywordsStoppingCriteria_mobileVLM
+from LLM.MobileVLM.mobilevlm.utils import disable_torch_init
+from LLM.mipha.serve.cli import load_image
+from LLM.SliME.llava.model.builder import load_pretrained_model as load_pretrained_model_LLaVAHD
+from LLM.SliME.llava.conversation import conv_templates as conv_templates_SLIME
+
+with open('config/config.json') as user_file:
+    config = user_file.read()
+config = json.loads(config)
+# {
+#   "name": "John",
+#   "age": 50,
+#   "is_married": false,
+#   "profession": null,
+#   "hobbies": ["travelling", "photography"]
+# }
+DEFAULT_LLM_MODEL = config["model_LLM_path"]
+
+class MobileVLM():
+    def __init__(self, model_path=DEFAULT_LLM_MODEL):
+        super().__init__()
+        model_name = model_path.split('/')[-1]
+        disable_torch_init()
+        tokenizer, model, image_processor, context_len = load_pretrained_model_mobileVLM(model_path, False,
+                                                                               False)
+        self.tokenizer = tokenizer
+        self.model = model
+        self.image_processor = image_processor
+        self.context_len = context_len
+
+        self.temperature = 0
+        self.top_p =  None
+        self.num_beams = 1
+        self.max_new_tokens = 512
+
+    @property
+    def metadata(self) -> LLMMetadata:
+        """Get LLM metadata."""
+        # 得到LLM的元数据
+        return LLMMetadata(
+            context_window=self.context_window,
+            num_output=self.num_output,
+            model_name=self.model_name,
+        )
+
+    @llm_completion_callback()  # 回调函数
+    def complete(self, prompt: str, image: str, **kwargs: Any) -> CompletionResponse:
+        pass
+
+    #  @llm_chat_callback()  # 回调函数
+    def chat(self, prompt, **kwargs: Any) -> ChatResponse:
+        # 完成函数
+        prompt, image = prompt[0], prompt[1]
+        conv = conv_templates_mobileVLM["v1"].copy()
+
+        if image is not None:
+            images = [Image.open(image).convert("RGB")]
+            images_tensor = process_images_mobileVLM(images, self.image_processor,self.model.config).to(self.model.device, dtype=torch.float16)
+            conv.append_message(conv.roles[0], DEFAULT_IMAGE_TOKEN + "\n" + prompt)
+        else:
+            images_tensor = None
+            conv.append_message(conv.roles[0],  prompt)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+        stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+        # Input
+        input_ids = (
+            tokenizer_image_token_mobileVLM(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).cuda())
+        stopping_criteria = KeywordsStoppingCriteria_mobileVLM([stop_str], self.tokenizer, input_ids)
+
+        with torch.inference_mode():
+            output_ids = self.model.generate(
+                input_ids,
+                images=images_tensor,
+                do_sample=True if self.temperature > 0 else False,
+                temperature=self.temperature,
+                top_p=self.top_p,
+                num_beams=self.num_beams,
+                max_new_tokens=self.max_new_tokens,
+                use_cache=True,
+                stopping_criteria=[stopping_criteria],
+            )
+        # Result-Decode
+        input_token_len = input_ids.shape[1]
+        n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        if n_diff_input_output > 0:
+            print(f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids")
+        outputs = self.tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        outputs = outputs.strip()
+        if outputs.endswith(stop_str):
+            outputs = outputs[: -len(stop_str)]
+        return outputs.strip()
+
+    @llm_completion_callback()
+    def stream_complete(
+            self, prompt: str, image: Optional[torch.FloatTensor] = None, **kwargs: Any
+    ) -> CompletionResponseGen:
+        pass
+class MiniCPM(CustomLLM):
+    context_window: int = 8192  # 上下文窗口大小
+    num_output: int = 128  # 输出的token数量
+    model_name: str = "MiniCPM"  # 模型名称
+    tokenizer: object = None  # 分词器
+    model: object = None  # 模型
+    image_processor: object = None # image_processor
+    def __init__(self, pretrained_model_name_or_path=DEFAULT_LLM_MODEL):
+        super().__init__()
+        # GPU方式加载模型
+        self.model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path, trust_remote_code=True,device_map="cuda:0" )
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path, trust_remote_code=True)
+
+    @property
+    def metadata(self) -> LLMMetadata:
+        """Get LLM metadata."""
+        # 得到LLM的元数据
+        return LLMMetadata(
+            context_window=self.context_window,
+            num_output=self.num_output,
+            model_name=self.model_name,
+        )
+
+    @llm_completion_callback()  # 回调函数
+    def complete(self, prompt: str, image: str, **kwargs: Any) -> CompletionResponse:
+        pass
+
+  #  @llm_chat_callback()  # 回调函数
+    def chat(self, prompt, **kwargs: Any) -> ChatResponse:
+        # 完成函数
+        prompt, image = prompt[0], prompt[1]
+        msgs = [{"content":prompt,"role":"user"}]
+        if image is not None:
+            image = Image.open(image).convert("RGB")
+        answer = self.model.chat(
+            image=image,
+            msgs=msgs,
+            context=None,
+            tokenizer=self.tokenizer,
+            sampling=True,
+            temperature=0.7
+        )
+        return answer
+
+    @llm_completion_callback()
+    def stream_complete(
+        self, prompt: str,image: Optional[torch.FloatTensor] = None, **kwargs: Any
+    ) -> CompletionResponseGen:
+        pass
+
+
+from LLM.SliME.llava.constants import IMAGE_PLACEHOLDER as IMAGE_PLACEHOLDER_SLIME
+from LLM.SliME.llava.mm_utils import tokenizer_image_token as tokenizer_image_token_SLIME
+class LLaVAHD(CustomLLM):
+    context_window: int = 8192  # 上下文窗口大小
+    num_output: int = 128  # 输出的token数量
+    model_name: str = "LLaVAHD"  # 模型名称
+    tokenizer: object = None  # 分词器
+    model: object = None  # 模型
+    image_processor: object = None # image_processor
+    def __init__(self, pretrained_model_name_or_path=DEFAULT_LLM_MODEL):
+        super().__init__()
+        # GPU方式加载模型
+        model_name = get_model_name_from_path_SLIME(pretrained_model_name_or_path)
+        self.model_name = model_name
+        tokenizer, model, image_processor, context_len = load_pretrained_model_LLaVAHD(pretrained_model_name_or_path, model_base=None, model_name=model_name)
+        self.tokenizer = tokenizer
+        self.model = model
+        self.image_processor = image_processor
+    @property
+    def metadata(self) -> LLMMetadata:
+        """Get LLM metadata."""
+        # 得到LLM的元数据
+        return LLMMetadata(
+            context_window=self.context_window,
+            num_output=self.num_output,
+            model_name=self.model_name,
+        )
+
+    @llm_completion_callback()  # 回调函数
+    def complete(self, prompt: str, image: str, **kwargs: Any) -> CompletionResponse:
+        pass
+
+  #  @llm_chat_callback()  # 回调函数
+    def chat(self, prompt, **kwargs: Any) -> ChatResponse:
+        # 完成函数
+        prompt, image = prompt[0], prompt[1]
+        image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
+        if IMAGE_PLACEHOLDER_SLIME in prompt:
+            if self.model.config.mm_use_im_start_end:
+                qs = re.sub(IMAGE_PLACEHOLDER_SLIME, image_token_se, prompt)
+            else:
+                qs = re.sub(IMAGE_PLACEHOLDER_SLIME, DEFAULT_IMAGE_TOKEN, prompt)
+        else:
+            if self.model.config.mm_use_im_start_end:
+                qs = image_token_se + "\n" + prompt
+            else:
+                qs = DEFAULT_IMAGE_TOKEN + "\n" + prompt
+
+
+        if "llama-2" in self.model_name.lower():
+            conv_mode = "llava_llama_2"
+        elif "mistral" in self.model_name.lower():
+            conv_mode = "mistral_instruct"
+        elif "v1.6-34b" in self.model_name.lower():
+            conv_mode = "chatml_direct"
+        elif "v1" in self.model_name.lower():
+            conv_mode = "llava_v1"
+        elif "mpt" in self.model_name.lower():
+            conv_mode = "mpt"
+        else:
+            conv_mode = "llava_v0"
+
+        conv = conv_templates_SLIME[conv_mode].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+        if image is not None:
+            # first message
+            image = load_image(image)
+            # image = image.resize((224, 224))
+            image_tensor = process_images([image], self.image_processor, self.model.config)
+            image_tensor = image_tensor.to(self.model.device, dtype=torch.float16)
+        else:
+            image_tensor = None
+        input_ids = tokenizer_image_token_SLIME(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(
+            0).cuda()
+        with torch.inference_mode():
+            outputs = self.model.generate(
+                input_ids,
+                images=image_tensor,
+                image_sizes = [image_tensor.shape] if image_tensor is not None else None,
+                do_sample=False,
+                temperature=0.2,
+                max_new_tokens=512,
+                num_beams=1,
+                use_cache=False,
+            )
+        outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0].strip()
+        return outputs
+
+    @llm_completion_callback()
+    def stream_complete(
+        self, prompt: str,image: Optional[torch.FloatTensor] = None, **kwargs: Any
+    ) -> CompletionResponseGen:
+        pass
+
+class OurLLM(CustomLLM):
+    context_window: int = 8192  # 上下文窗口大小
+    num_output: int = 128  # 输出的token数量
+    model_name: str = "Mipha"  # 模型名称
+    tokenizer: object = None  # 分词器
+    model: object = None  # 模型
+    image_processor: object = None # image_processor
+    def __init__(self, pretrained_model_name_or_path=DEFAULT_LLM_MODEL):
+        super().__init__()
+        # GPU方式加载模型
+        tokenizer, model, image_processor, context_len = load_pretrained_model(pretrained_model_name_or_path, model_base=None, model_name="Mipha-phi2")
+        self.tokenizer = tokenizer
+        self.model = model
+        self.image_processor = image_processor
+
+    @property
+    def metadata(self) -> LLMMetadata:
+        """Get LLM metadata."""
+        # 得到LLM的元数据
+        return LLMMetadata(
+            context_window=self.context_window,
+            num_output=self.num_output,
+            model_name=self.model_name,
+        )
+
+    @llm_completion_callback()  # 回调函数
+    def complete(self, prompt: str, image: str, **kwargs: Any) -> CompletionResponse:
+        pass
+
+  #  @llm_chat_callback()  # 回调函数
+    def chat(self, prompt, **kwargs: Any) -> ChatResponse:
+        # 完成函数
+        prompt, image = prompt[0], prompt[1]
+        conv = conv_templates["phi"].copy()
+        roles = conv.roles
+        inp = f"{roles[0]}: {prompt}"
+
+        streamer = TextStreamer(self.tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+        if image is not None:
+            # first message
+            image = load_image(image)
+            # image = image.resize((224, 224))
+            image_tensor = process_images([image], self.image_processor, self.model.config)
+            image_tensor = image_tensor.to(self.model.device, dtype=torch.float16)
+            if self.model.config.mm_use_im_start_end:
+                inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+            else:
+                inp = DEFAULT_IMAGE_TOKEN + '\n' + inp
+            conv.append_message(conv.roles[0], inp)
+        else:
+            # later messages
+            conv.append_message(conv.roles[0], inp)
+            image_tensor = None
+
+        conv.append_message(conv.roles[1], None)
+
+        prompt = conv.get_prompt()
+        input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(
+            0).cuda()
+        stop_str = conv.sep2
+        keywords = [stop_str]
+        stopping_criteria = KeywordsStoppingCriteria(keywords, self.tokenizer, input_ids)
+        with torch.inference_mode():
+            outputs = self.model.generate(
+                input_ids,
+                images=image_tensor,
+                do_sample=False,
+                temperature=0,
+                max_new_tokens=1024,
+                streamer=streamer,
+                use_cache=True,
+                eos_token_id=self.tokenizer.eos_token_id,  # End of sequence token
+                pad_token_id=self.tokenizer.eos_token_id,  # Pad token
+                stopping_criteria=[stopping_criteria]
+            )
+        outputs = self.tokenizer.decode(outputs[0, input_ids.shape[1]:]).strip()
+        return outputs
+
+    @llm_completion_callback()
+    def stream_complete(
+        self, prompt: str,image: Optional[torch.FloatTensor] = None, **kwargs: Any
+    ) -> CompletionResponseGen:
+        pass
---
+++ multimodal_rag-main_v1-master/server.py
@@ -0,0 +1,293 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import shutil
+import traceback
+
+import gradio as gr
+from gradio.components.chatbot import FileMessage
+from gradio.data_classes import FileData
+
+from build_rag import retrieve, get_retriever_engine, retrieve_image_to_image, get_retriever_engine_from_local, \
+    retrieve_json
+from PIL import Image
+import numpy as np
+import sys
+from model_LLM import OurLLM, MiniCPM, MobileVLM, LLaVAHD
+
+model_name = sys.argv[1]
+if model_name == "MobileVLM":
+    llm = MobileVLM()
+elif model_name == "MiniCPM":
+    llm = MiniCPM()
+elif model_name == "LLaVA-HD":
+    llm = LLaVAHD()
+
+def complex_analysis(retriever_engine_dict, query_str, image, image_file_name):
+    retriever_engine = retriever_engine_dict["retriever_engine"]
+    json_response = retrieve_json(retriever_engine_dict["json_engine"],query_str)
+    img = []
+    txt = []
+    if query_str != "":
+        img, txt = retrieve(retriever_engine, query_str)
+    if image is not None and img == []:
+        # image 检索image只取image检索结果，忽略文本检索结果
+        print("image 检索image只取image检索结果，忽略文本检索结果")
+        image = Image.open(io.BytesIO(base64.b64decode(image))).convert('RGB')
+        # 保存Image对象为文件
+        image.save("storage/cache/%s" % (image_file_name))
+        img1, txt1 = retrieve_image_to_image(retriever_engine, "storage/cache/%s"%(image_file_name))
+        img = img1
+        txt = txt1
+
+    # 只取排序后每个子列表的第一个元素
+    if txt != []:
+        txt = sorted(txt, key=lambda x: x[1], reverse=True)
+        print(txt)
+        txt = [item[0] for item in txt][0]
+        context_str = txt
+        print("context_str", context_str)
+    else:
+        context_str = "没有上下文，直接回答"
+    if img == []:
+        return context_str, None, None
+    img = sorted(img, key=lambda x: x[1], reverse=True)
+    img = [item[0] for item in img]
+    return context_str +" json:" + str(json_response), img, img[0]
+
+def call_LLM(prompt, image_path, image=None, retriever_img_path=None):
+
+    if image is not None and retriever_img_path is None:
+        image = Image.open(io.BytesIO(base64.b64decode(image))).convert('RGB')
+        image.save(image_path)
+
+    if image is not None and retriever_img_path is not None:
+        image1 = Image.open(io.BytesIO(base64.b64decode(image))).convert('RGB')
+        image2 = Image.open(retriever_img_path)
+        # Calculate the width and height of the new image
+        new_width = image1.width + image2.width
+        new_height = max(image1.height, image2.height)
+        # Create a new image with the calculated dimensions
+        new_image = Image.new('RGB', (new_width, new_height))
+        # Paste images into the new image
+        new_image.paste(image1, (0, 0))
+        new_image.paste(image2, (image1.width, 0))
+        # Save the new image
+        new_image.save(image_path)
+
+    if model_name == "MiniCPM" or model_name == "MiniCPM2":
+        response = llm.chat({0: prompt, 1:image_path})[0]
+        response = response.replace("<|endoftext|>","")
+    else:
+        response = llm.chat({0: prompt, 1:image_path}).replace("<|endoftext|>","")
+    return response
+
+import base64
+import io
+import os.path
+import gradio as gr
+
+from PIL import Image
+
+import zipfile
+def upload_img(file, _chatbot, _app_session):
+    _app_session['sts'] = None
+    _app_session['ctx'] = []
+    with open(file, 'rb') as f1:
+        base64_str = base64.b64encode(f1.read()) # str类型
+    _app_session['file'] = base64_str
+    _app_session['file_name'] = os.path.basename(file)
+    return _chatbot, _app_session
+
+def unzip(zip_file, output=None):
+    """解压zip文件"""
+    if not os.path.exists(output):
+        os.mkdir(output)
+    output = output or os.path.dirname(zip_file)  # 默认解压到当前目录同名文件夹中
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        for zip_info in zip_ref.infolist():
+            # 解决中文文件名乱码问题
+            zip_info.filename = zip_info.filename.encode('cp437').decode('gbk')
+            zip_ref.extract(zip_info, output)
+
+def load_knowledge(_app_cfg, _chat_bot):
+    if _app_cfg['ret'] is not None:
+        _app_cfg['ret']['client'].close()
+    code, message = get_retriever_engine_from_local()
+    if code == 200:
+        _app_cfg['ret'] = message
+        _app_cfg['message'] = "success"
+        _chat_bot.append(('', '知识库更新成功，现在你可以和我聊天啦！'))
+        return "success", _app_cfg, _chat_bot
+    else:
+        _app_cfg['ret'] = None
+        _app_cfg['message'] = message
+        _chat_bot.append(('', '知识库更新失败，原因：%s' % message))
+        return message, _app_cfg, _chat_bot
+
+def respond(_app_cfg, _chat_bot, image_folder, text_folder):
+    if _app_cfg['file'] is None:
+        _app_cfg['ret'] = "您还没上传任何文件"
+        return '', _app_cfg, _chat_bot
+
+    debase = base64.b64decode(_app_cfg['file'])
+    with open("storage/"+_app_cfg["file_name"], 'wb') as f1:
+        f1.write(debase)  # str类型
+    if _app_cfg["file_name"].endswith(".zip"):
+        unzip("storage/"+_app_cfg["file_name"],"storage/decompress")
+
+    if _app_cfg['ret'] is not None and not isinstance(_app_cfg['ret'], str):
+        _app_cfg['ret']["client"].close()
+    code, message = get_retriever_engine("storage/decompress", image_folder, text_folder)
+    if code == 200:
+        _app_cfg['ret'] = message
+        _app_cfg['message'] = "success"
+        _chat_bot.append(('', '知识库更新成功，现在你可以和我聊天啦！'))
+        return "success", _app_cfg, _chat_bot
+    else:
+        _app_cfg['ret'] = None
+        _app_cfg['message'] = message
+        _chat_bot.append(('', '知识库更新失败，原因：%s'%message))
+        return message, _app_cfg, _chat_bot
+
+def respond1(message, _chat_bot, _app_cfg, prompt):
+
+    if _app_cfg['ctx'] is None:
+        _app_cfg['ctx'] = []
+
+    base64_str = None
+    image_file_name = None
+    if message['files'] is not None:
+        for item in message['files']:
+            with open(item, 'rb') as f1:
+                base64_str = base64.b64encode(f1.read()) # str类型
+                image_file_name = os.path.basename(item)
+    _question = message["text"]
+    _context = _app_cfg['ctx'].copy()
+    if _context:
+        _context.append({"role": "user", "content": _question})
+    else:
+        _context = [{"role": "user", "content": _question}]
+
+    print('<User>:', _question)
+    if _app_cfg['ret'] is None:
+        message_by_index, _app_cfg, _chat_bot = load_knowledge(_app_cfg, _chat_bot)
+
+    if _app_cfg['ret'] is None:
+        if image_file_name is None:
+            _answer = call_LLM(_question,None)
+            _app_cfg['ctx'] = _context
+            _app_cfg['sts'] = 200
+            _context.append({"role": "assistant", "content": _answer})
+            _chat_bot.append((_question, _answer))
+
+        else:
+            _answer = call_LLM(_question,image_path=os.path.join("storage","cache", image_file_name),image=base64_str)
+            _app_cfg['ctx'] = _context
+            _app_cfg['sts'] = 200
+            _app_cfg['img'] = os.path.join("storage", "cache", image_file_name)
+            _context.append({"role": "assistant", "content": _answer, "img": _app_cfg['img']})
+            _chat_bot.append(([_app_cfg['img'].__str__(), _question], _answer))
+        print('<Assistant>:', _answer)
+
+        #_chat_bot.append(("", [_app_cfg['img'].__str__(), _answer]))
+        message["text"] = ""
+        message['files'] = None
+        return gr.MultimodalTextbox(interactive=True, file_types=["image"],
+                                      placeholder="输入消息或者上传图片", show_label=False), _chat_bot, _app_cfg
+
+    _knowledge, img, img_path = complex_analysis(_app_cfg['ret'], _question, base64_str, image_file_name)
+    try:
+        print("指令:",prompt + "  上下文是:"+_knowledge +"  问题是：" +_question )
+        _answer = call_LLM("Prompt:"+prompt+". Context:"+_knowledge +". Question:" +_question, img_path)
+    except Exception as e:
+        traceback.print_exc()
+        _answer = "调用大模型出现错误，错误原因: %s"%(e.__str__())
+    _context.append({"role": "assistant", "content": _answer,"img": os.path.relpath(img_path) if img_path is not None else None})
+    _app_cfg['ctx'] = _context
+    _app_cfg['sts'] = 200
+    _app_cfg['img'] = os.path.relpath(img_path) if img_path is not None else None
+    print('<Assistant>:', _answer)
+    message["text"] = ""
+    message['files'] = None
+
+    if img_path is not None:
+        if image_file_name is not None:
+            _chat_bot.append(([_app_cfg['img'].__str__(), ""], None))
+        _chat_bot.append((_question, [os.path.relpath(img_path).__str__(),""]))
+        _chat_bot.append((None, _answer))
+    else:
+        if image_file_name is not None:
+            _chat_bot.append(([_app_cfg['img'].__str__(), ""], None))
+        _chat_bot.append((_question, _answer))
+
+    return gr.MultimodalTextbox(interactive=True, file_types=["image"],
+                                      placeholder="输入消息或者上传图片", show_label=False), _chat_bot, _app_cfg
+
+def regenerate_button_clicked( _chat_bot, _app_cfg, image_folder, text_folder):
+    message, app_session,_chat_bot = respond(_app_cfg, _chat_bot, image_folder, text_folder)
+    return _chat_bot, app_session
+
+def delete_know_base(_app_cfg, _chat_bot):
+    if _app_cfg['ret'] is not None:
+        _app_cfg['ret']["client"].close()
+    shutil.rmtree("storage/decompress")
+    shutil.rmtree("qdrant_mm_db/collection")
+    _chat_bot.append(('', '知识库删除成功。'))
+    return _app_cfg, _chat_bot
+
+def create_component(params):
+    return gr.Button(
+        value=params['value'],
+        interactive=True
+    )
+
+with gr.Blocks() as funclip_service:
+    with gr.Column():
+        with gr.Row():
+            bt_file = gr.File(label="上传知识库 .zip 格式文件")
+            konwbase = create_component({'value': '向量化'})
+            rmkonwbase = create_component({'value': '删除知识库'})
+
+        with gr.Column():
+            image_folder = gr.Textbox(label="image_folder",
+                                value="image",
+                                interactive=True)
+
+            text_folder = gr.Textbox(label="text_folder",
+                                value="text",
+                                interactive=True)
+
+        with gr.Column():
+            prompt = gr.Textbox(label="prompt",
+                                value="""You're a navigation assistant robot. Based on the information and questions, respond with coordinates if the text includes object coordinates (x, y). And a json, "name" is room name,"top_left" and "bottom_right" is the room range, "aps" is access point, "object" listed object in room.""",
+                                interactive=True)
+            chat_input = gr.MultimodalTextbox(interactive=True, file_types=["image"],
+                                      placeholder="输入消息或者上传图片", show_label=False)
+
+    with gr.Column():
+        with gr.Row():
+            app_session = gr.State({'sts': None, 'ctx': None, 'img': None,'ret':None,'file':None})
+            chat_bot = gr.Chatbot(label=f"Chat with {model_name}")
+
+    konwbase.click(
+        regenerate_button_clicked,
+       [chat_bot, app_session, image_folder, text_folder],
+        [chat_bot, app_session]
+    )
+    rmkonwbase.click(
+        delete_know_base,
+        [app_session, chat_bot],
+        [app_session, chat_bot]
+    )
+    chat_input.submit(
+        respond1,
+        [chat_input, chat_bot, app_session, prompt],
+        [chat_input, chat_bot, app_session]
+    )
+    bt_file.upload(lambda: None, None, chat_bot, queue=False).then(upload_img,
+                                                                  inputs=[bt_file, chat_bot, app_session],
+                                                                  outputs=[chat_bot, app_session])
+
+if __name__ == '__main__':
+    funclip_service.launch(share=False)
